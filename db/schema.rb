b93924190427e17b45f736474ee0da42eb134f8a
Jahaira <jahaira.marrero@gmail.com>
2021-05-10 14:08:49 -0400

    instructor image and show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_194109) do
+ActiveRecord::Schema.define(version: 2021_05_10_163315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_194109) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "students", force: :cascade do |t|
